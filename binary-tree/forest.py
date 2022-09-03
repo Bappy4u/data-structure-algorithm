992fcb91f67be7334c443e53f97bd8aeeff4a632
Najmul H. Bappy <http.bappy@gmail.com>
2022-09-03 12:32:36 +0600

    updated

--- binary-tree/forest.py
+++ binary-tree/forest.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,id) -> None:
         self.id = id
         self.children = []
-        self.parent = None
+        self.parent = -1
     def addChild(self,node):
         if node not in self.children:
             self.children.append(node)
@@ -11,42 +11,39 @@ class Node:
 
        
 
-list = [[1,0],[2,1],[3,1],[4,1],[6,1],[7,1],[8,2],[9,5],[10,11]]
+list = [[0,-1],[1,0],[2,1],[3,1],[4,1],[6,1],[7,1],[8,2],[9,5],[10,11]]
 
 roots = []
 
 hash = {}
 def find_or_create(nid):
-    if hash.get(nid):
-        return hash[nid]
-    else:
-        return Node(nid)
+    if not hash.get(nid):
+        hash[nid] = Node(nid)
+    return hash[nid]
 
 
 for id,parentId in list:
-
     child_node = find_or_create(id)
+    if parentId==-1:
+        roots.append(child_node)
+        continue
+    
     parent_node = find_or_create(parentId)
 
-    parent_node.addChild(child_node)
-    child_node.parent = parent_node
+    parent_node.addChild(id)
+    child_node.parent = parentId
    
-    hash[id]=child_node
-    hash[parentId] = parent_node
-    if parent_node not in roots:
+    
+    if parent_node not in roots and parent_node.parent ==-1:
         roots.append(parent_node)
     if child_node in roots:
         roots.remove(child_node)
 
 
 
-
-    
-
-
-
+print(len(roots))
 
 for root in roots:
     print(root)
     for node in root.children:
-        print("\t", node)
+        print("\t", hash[node])
